021e9adca863b73e73287f306d7656124c663257
Garnet Yang <yangarnet@gmail.com>
2018-09-22 22:07:41 +1000

    rename react component with jsx

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -3,58 +3,36 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     entry: {
-        index: './src/index.js',
+        index: './src/index.js'
     },
     plugins: [
         new CleanWebpackPlugin(['index']),
         new HtmlWebpackPlugin({
-        title: 'Minimum-Viable',
-        filename: 'index.html',
-        template: './public/index.html',
+            title: 'Minimum-Viable',
+            filename: 'index.html',
+            template: './public/index.html'
         })
     ],
     module: {
         rules: [
-        {
-            test: /\.jsx?$/,
-            use: {
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                'es2015',
-                'react',
-                ],
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: 'babel-loader'
+                },
+                exclude: /node_modules/
             },
-            },
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.js$/,
-            use: {
-            loader: 'babel-loader',
-            options: {
-                presets: [
-                'es2015',
-                'react',
-                ],
-            },
-            },
-            exclude: /node_modules/,
-        },
-        {
-            test: /\.css$/,
-            use: [
-            'style-loader',
-            'css-loader',
-            ],
-        },
-        ],
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     },
     resolve: {
         extensions: ['.js', '.jsx', '.json']
     },
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist')
     }
 };
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,21 +3,11 @@ const common = require('./webpack.config.common.js');
 const webpack = require('webpack');
 const path = require('path');
 module.exports = merge(common, {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  devServer: {
-    hot: true,
-    contentBase: path.resolve(__dirname, 'index'),
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        use: 'react-hot-loader'
-      }
-    ]
-  }
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        hot: true,
+        contentBase: path.resolve(__dirname, 'index')
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()]
 });
